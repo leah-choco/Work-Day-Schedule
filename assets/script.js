466860c20b7829602d32e4cd7e7ba34dfe1a3ae5
Your Name <you@example.com>
2022-08-29 16:10:52 -0500

    Trying functions to change colors

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,48 @@
-$("#currentDay").text(moment().format("MMM Do YY"))
 
 
+var currentTime = moment().hour();
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+
+function timeBlockColors() {
+    $(".time-block").each(function() {
+        var blockColors = parseInt($(this).attr("id").split("hour") [1]);
+        
+        if (blockColors < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+
+        }
+        else if (blockColors === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else{
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    
+    })
+    return
+}
+
+
+
+
+
+
+//Event Listener for Save Button
+$(".saveBtn").on("click", function(){
+    console.log(this);
+
+
+})
+
+
+//Saving textarea input to localStorage
 localStorage.setItem("mytest", "hello world")
 console.log(localStorage.getItem("mytest"))
