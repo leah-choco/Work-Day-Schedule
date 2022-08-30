110d326c3ec6dd00cf7349a9eec115daf5636f55
Your Name <you@example.com>
2022-08-30 16:32:48 -0500

    added localStorage and all working properly

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,28 @@ var currentTime = moment().hour();
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
+//Event Listener for Save Button
+$(".saveBtn").on("click", function(){
+    console.log(this);
+   var text = $(this).siblings(".description").val();
+   var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time,text);
+
+})
+
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+
 function timeBlockColors() {
     $(".time-block").each(function() {
         var blockColors = parseInt($(this).children().eq(1).attr("id"));
@@ -25,24 +47,16 @@ function timeBlockColors() {
             $(this).removeClass("present");
             $(this).addClass("future");
         }
-    
+        
     })
     return
 }
 
+timeBlockColors();
 
 
 
 
 
-//Event Listener for Save Button
-$(".saveBtn").on("click", function(){
-    console.log(this);
 
 
-})
-
-
-//Saving textarea input to localStorage
-localStorage.setItem("mytest", "hello world")
-console.log(localStorage.getItem("mytest"))
