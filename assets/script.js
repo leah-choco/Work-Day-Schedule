1e7679a3221055d20a7f107c320293478018d5df
Your Name <you@example.com>
2022-08-31 15:15:31 -0500

    final comments and cleaning up the code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
-
-
 var currentTime = moment().hour();
 
-
+//Displaying Current Date and Time 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
 //Event Listener for Save Button
@@ -15,6 +13,7 @@ $(".saveBtn").on("click", function(){
 
 })
 
+//LocalStorage displaying data input when page is reloaded
 $("#8").val(localStorage.getItem("8"));
 $("#9").val(localStorage.getItem("9"));
 $("#10").val(localStorage.getItem("10"));
@@ -26,7 +25,7 @@ $("#15").val(localStorage.getItem("15"));
 $("#16").val(localStorage.getItem("16"));
 $("#17").val(localStorage.getItem("17"));
 
-
+//Color Coded Blocks based on the current time
 function timeBlockColors() {
     $(".time-block").each(function() {
         var blockColors = parseInt($(this).children().eq(1).attr("id"));
@@ -49,10 +48,6 @@ function timeBlockColors() {
         }
         
     })
- 
- 
- 
-
  
 }
 
