f42724cbde067b5eb341e97e663f90f138fbbedd
Your Name <you@example.com>
2022-08-31 13:11:44 -0500

    Defined time in lS, but text doesn't persist

--- assets/script.js
+++ assets/script.js
@@ -9,22 +9,22 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 $(".saveBtn").on("click", function(){
     console.log(this);
    var text = $(this).siblings(".description").val();
-   var time = $(this).parent().attr("id");
+   var time = $(this).siblings(".description").attr("id");
 
     localStorage.setItem(time,text);
 
 })
 
 $("#8 .description").val(localStorage.getItem("8"));
-//$("#9 .description").val(localStorage.getItem("9"));
-//$("#10 .description").val(localStorage.getItem("10"));
-//$("#11 .description").val(localStorage.getItem("11"));
-//$("#12 .description").val(localStorage.getItem("12"));
-//$("#13 .description").val(localStorage.getItem("13"));
-//$("#14 .description").val(localStorage.getItem("14"));
-//$("#15 .description").val(localStorage.getItem("15"));
-//$("#16 .description").val(localStorage.getItem("16"));
-//$("#17 .description").val(localStorage.getItem("17"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 function timeBlockColors() {
