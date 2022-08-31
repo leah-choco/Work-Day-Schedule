c76c029cfc046f0d4cee19bf23452b3c28741f4e
Your Name <you@example.com>
2022-08-31 12:45:47 -0500

    Debugging localStorage

--- assets/script.js
+++ assets/script.js
@@ -16,20 +16,20 @@ $(".saveBtn").on("click", function(){
 })
 
 $("#8 .description").val(localStorage.getItem("8"));
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-$("#17 .description").val(localStorage.getItem("17"));
+//$("#9 .description").val(localStorage.getItem("9"));
+//$("#10 .description").val(localStorage.getItem("10"));
+//$("#11 .description").val(localStorage.getItem("11"));
+//$("#12 .description").val(localStorage.getItem("12"));
+//$("#13 .description").val(localStorage.getItem("13"));
+//$("#14 .description").val(localStorage.getItem("14"));
+//$("#15 .description").val(localStorage.getItem("15"));
+//$("#16 .description").val(localStorage.getItem("16"));
+//$("#17 .description").val(localStorage.getItem("17"));
 
 
 function timeBlockColors() {
     $(".time-block").each(function() {
-        var blockColors = parseInt($(this).attr("id").split("hour")[1]);
+        var blockColors = parseInt($(this).children().eq(1).attr("id"));
         
         if (blockColors < currentTime) {
             $(this).addClass("past");
@@ -50,8 +50,8 @@ function timeBlockColors() {
         
     })
  
- return
-
+ 
+ 
 
  
 }
